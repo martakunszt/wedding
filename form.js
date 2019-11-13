fa5c8eb60e7a5335f6596cc144228e7b9f923461
martakunszt <kunsztowicz.m@gmail.com>
2019-11-13 16:43:30 +0000

    anything

---
+++ form.js
@@ -0,0 +1,14 @@
+//set up variables
+
+let name = document.getElementById('name');
+let seccondname = document.getElementById('surname');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+let msg = document.getElementById('message');
+let submit = document.getElementById('submit');
+
+function add(){
+    
+}
+
+submit.addEventListener('click', add);
