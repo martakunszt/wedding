0116714e87bb6e1ec42049446235f5196829f9a9
martakunszt <kunsztowicz.m@gmail.com>
2019-11-14 09:40:43 +0000

    JS

--- shop.js
+++ shop.js
@@ -1,8 +1,8 @@
 var button1 = document.getElementById('button1');
-
+var name1 = document.getElementById('product1')
 function one(){
     var node = document.createElement('LI');
-    var textnode = document.createTextNode("Flowers");
+    var textnode = document.createTextNode(name1.textContent);
     node.appendChild(textnode);
     document.getElementById('basket').appendChild(node);
 }
